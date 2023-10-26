d7dc4b29376899d1ffb821d721545db784e9f07b
sabita <sabitakhadka091@gmail.com>
2023-10-26 11:10:16 +0545

    added new components with new different functionality

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 // import App from './App';
 import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating}/>
+      <p>This movie was rating {movieRating} stars</p>
+    </div>
+  );
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
-    <StarRating maxRating={10} />
-    <StarRating />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={30} color="red" className="test" defaultRating={3} />
+    <Test />
   </React.StrictMode>
 );
