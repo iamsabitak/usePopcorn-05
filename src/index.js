f1f225af2f1a59089ca595c21ceb491fa43c9992
sabita <sabitakhadka091@gmail.com>
2023-10-27 10:02:45 +0545

    new component implemneted

--- src/index.js
+++ src/index.js
@@ -1,27 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import App from './App';
-import StarRating from "./StarRating";
+import App from './App';
+// import StarRating from "./StarRating";
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-  return (
-    <div>
-      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating}/>
-      <p>This movie was rating {movieRating} stars</p>
-    </div>
-  );
-}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
     <StarRating size={30} color="red" className="test" defaultRating={3} />
-    <Test />
+     */}
+   
   </React.StrictMode>
 );
