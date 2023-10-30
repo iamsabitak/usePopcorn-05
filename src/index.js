905f85c9704b1557ef7fea77e3e58644210f8e7a
sabita <sabitakhadka091@gmail.com>
2023-10-30 14:20:06 +0545

    remove starrating component adn implemneted app component again

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import { useState } from "react";
+import { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from './App';
