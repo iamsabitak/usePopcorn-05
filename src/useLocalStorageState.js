9dfa11bce68acb15e8bce609ddc12c5c5cbf53f0
sabita <sabitakhadka091@gmail.com>
2023-11-02 13:08:45 +0545

    Creating useLocalStorageState

--- src/useLocalStorageState.js
+++ src/useLocalStorageState.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 
 export function useLocalStorageState(initialState, key) {
+  
   const [value, setValue] = useState(function () {
     const storedValue = localStorage.getItem(key);
     return storedValue ? JSON.parse(storedValue) : initialState;
