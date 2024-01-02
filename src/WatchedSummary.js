9b1c7bdbd603c25bd3454c9089b64f247ae74f1d
sabita <sabitakhadka091@gmail.com>
2024-01-02 15:49:11 +0545

    Enhance Movie Summary Display

--- src/WatchedSummary.js
+++ src/WatchedSummary.js
@@ -23,7 +23,7 @@ export function WatchedSummary({ watched }) {
         </p>
         <p>
           <span>⏳</span>
-          <span>{avgRuntime} min</span>
+          <span>{avgRuntime.toFixed(2)} min</span>
         </p>
       </div>
     </div>
